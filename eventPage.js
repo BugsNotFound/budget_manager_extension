560ecda56146b6016ee9d7e7b1942da1c5c77dc7
Himansh Jain <pctech4uproduction@gmail.com>
2019-08-14 04:37:23 +0530

    Added event Pages context menu

---
+++ eventPage.js
@@ -0,0 +1,43 @@
+var contextMenuItem = {
+    "id": "SpendMoney",
+    "title": "SpendMoney",
+    "contexts": ["selection"]
+};
+
+chrome.contextMenus.create(contextMenuItem);
+
+function isInt(value) {
+    return !isNaN(value) &&
+        parseInt(Number(value)) == value &&
+        !isNaN(parseInt(value, 10));
+}
+
+chrome.contextMenus.onClicked.addListener(function(clickedData) {
+    if (clickedData.menuItemId == "SpendMoney" && clickedData.selectionText) {
+        if (isInt(clickedData.selectionText)) {
+            chrome.storage.sync.get(["total", "limit"], function(budget) {
+                var newTotal = 0;
+                if (budget.total) {
+                    newTotal += parseInt(budget.total);
+                }
+
+
+
+                newTotal += parseInt((clickedData.selectionText).split(',').join(''));
+
+                chrome.storage.sync.set({ "total": newTotal }, function() {
+                    if (newTotal >= budget.limit) {
+                        var notifOptions = {
+                            type: "basic",
+                            iconUrl: "icon48.png",
+                            title: "Limit reached!",
+                            message: "Uh oh! Looks like you've reached your limit"
+                        };
+
+                        chrome.notifications.create("limitNotif", notifOptions);
+                    }
+                });
+            });
+        }
+    }
+});
--- options.js
+++ options.js
@@ -7,15 +7,32 @@ $(function() {
         var limit = $("#limit").val();
         if (limit) {
             chrome.storage.sync.set({ "limit": limit }, function() {
-                alert("Limit set!");
-                close();
+                var notifOptions = {
+                    type: "basic",
+                    iconUrl: "icon48.png",
+                    title: "Limit set!",
+                    message: "Limit has been updated!"
+                };
+
+                chrome.notifications.create("limitUpdateNotif", notifOptions, function() {
+                    close();
+                });
             });
         }
     });
 
     $("#resetTotal").click(function() {
-        chrome.storage.sync.set({ "total": 0 });
-        alert("Total spent value set to 0!");
-        close();
+        chrome.storage.sync.set({ "total": 0 }, function() {
+            var notifOptions = {
+                type: "basic",
+                iconUrl: "icon48.png",
+                title: "Total reset!",
+                message: "Total has been reset to 0!"
+            };
+
+            chrome.notifications.create("totalReset", notifOptions, function() {
+                close();
+            });
+        });
     });
 });
